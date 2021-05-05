5221d445c2d4472c8def550b647d263ebd7c9a65
greinisch <greinisch@youwol.com>
2021-05-05 07:05:43 +0200

    :lipstick:

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,14 +1,5 @@
 import { FluxPack, IEnvironment } from '@youwol/flux-core'
 import { AUTO_GENERATED } from '../auto_generated'
 
-export function install(environment: IEnvironment){
-    return
-}
-
-export let pack = new FluxPack({
-    ...AUTO_GENERATED,
-    ...{
-        install
-    }
-})
+export let pack = new FluxPack(AUTO_GENERATED)
 
